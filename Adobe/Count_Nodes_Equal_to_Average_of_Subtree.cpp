6e527eff5fe5c57c24ce4d04a76be20c17659f23
amankr027 <amanvermay27@gmail.com>
2023-01-14 22:47:25 +0530

    Question 9

---
+++ Adobe/Count_Nodes_Equal_to_Average_of_Subtree.cpp
@@ -0,0 +1,47 @@
+/*
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std; 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int,int>find(TreeNode*root , int&sum)
+    {
+        if(root==NULL)
+        {
+            //first sum of subtree
+            // second size of subtree
+            return {0,0};
+        }
+
+        auto left=find( root->left  , sum);
+        auto right=find( root->right , sum);
+        int sum_of_subtree=root->val+left.first+right.first;
+        int size_of_subtree=1+left.second+right.second;
+        if((sum_of_subtree)/(size_of_subtree)==root->val)
+        {
+            sum++;
+        }
+        return {sum_of_subtree,size_of_subtree};
+    }
+    int averageOfSubtree(TreeNode* root) 
+    {
+        int sum=0;
+        find(root,sum);
+        return sum;
+    }
+};
+
