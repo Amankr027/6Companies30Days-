56b411ba66053ee051ebd721a2adf80461e7523a
amankr027 <amanvermay27@gmail.com>
2023-01-15 22:01:29 +0530

    Question 5

---
+++ Adobe/Non-negative_Integers_without_Consecutive_Ones.cpp
@@ -0,0 +1,50 @@
+/*
+Given a positive integer n, return the number of the integers in the range [0, n] whose binary representations do not contain consecutive ones.
+
+ 
+
+Example 1:
+
+Input: n = 5
+Output: 5
+Explanation:
+Here are the non-negative integers <= 5 with their corresponding binary representations:
+0 : 0
+1 : 1
+2 : 10
+3 : 11
+4 : 100
+5 : 101
+Among them, only integer 3 disobeys the rule (two consecutive ones) and the other 5 satisfy the rule. 
+Example 2:
+
+Input: n = 1
+Output: 2
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+  int findIntegers(int n) {
+    int f[31];
+    f[0] = 1;
+    f[1] = 2;
+    for (int i = 2; i < 31; ++i)
+      f[i] = f[i-1]+f[i-2];
+
+    int ans = 0, k = 30, pre_bit = 0;
+    while (k >= 0) {
+      if (n&(1<<k)) {
+        ans += f[k];
+        if (pre_bit) return ans;
+        pre_bit = 1;
+      }else {
+        pre_bit = 0;
+      }
+      --k;
+    }
+    return ans+1;
+  }
+};
